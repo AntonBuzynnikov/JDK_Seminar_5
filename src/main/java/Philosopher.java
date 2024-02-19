6fe2a20d76bc18465b693cd8527fd588050a6611
Anton <buzynnikov2111@gmail.com>
2024-02-19 21:01:07 +0500

    Complete 3.0

--- src/main/java/Philosopher.java
+++ src/main/java/Philosopher.java
@@ -6,12 +6,14 @@ public class Philosopher implements Runnable{
     private int index;
     private int countEating = 0;
     private boolean eat;
+    private boolean think;
 
     public Philosopher(Fork forkLeft, Fork forkRight, int index) {
         this.forkLeft = forkLeft;
         this.forkRight = forkRight;
         this.index = index;
         eat = false;
+        think = true;
     }
 
     @Override
@@ -32,7 +34,7 @@ public class Philosopher implements Runnable{
                 forkRight.setState("Free");
             } else{
                 System.out.println("Философ № " + index + " размышляет");
-                eat = !eat;
+                if(eat) eat = false;
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
